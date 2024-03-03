8acdc2538591204d01b3c9d6ad75b4bdb91cc415
ishakalpi <153492730+ishakalpi@users.noreply.github.com>
2024-03-03 13:47:28 +0530

    settings for project

--- RegisterLogin/RegisterLogin/Models/ErrorViewModel.cs
+++ RegisterLogin/RegisterLogin/Models/ErrorViewModel.cs
@@ -1,8 +1,9 @@
-namespace RegisterLogin.Models;
-
-public class ErrorViewModel
+namespace RegisterLogin.Models
 {
-    public string? RequestId { get; set; }
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
 }
